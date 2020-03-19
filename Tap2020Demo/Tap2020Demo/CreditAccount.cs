93fdadba8982f00f12cdbdb2e02909a8d623e40d
RePierre <petru.rebeja@gmail.com>
2020-03-19 17:27:23 +0200

    Initial setup of the demo

---
+++ Tap2020Demo/Tap2020Demo/CreditAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace Tap2020Demo
+{
+    class CreditAccount : Account
+    {
+        protected override decimal CalculateWithdrawalFee(decimal amount)
+        {
+            return amount * 0.2m;
+        }
+    }
+}
---
+++ Tap2020Demo/Tap2020Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tap2020Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account debitAccount = new DebitAccount();
+            debitAccount.Deposit(100);
+            debitAccount.Withdraw(50);
+            Console.WriteLine("Debit account: {0}.", debitAccount.Amount);
+
+            Account creditAccount = new CreditAccount();
+            creditAccount.Deposit(100);
+            creditAccount.Withdraw(50);
+            Console.WriteLine("Credit account: {0}.", creditAccount.Amount);
+        }
+    }
+}
