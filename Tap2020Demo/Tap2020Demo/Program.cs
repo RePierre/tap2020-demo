a369dbc5e45b17ecc5d3756a57ed9dd518bc6170
RePierre <petru.rebeja@gmail.com>
2020-03-19 17:49:28 +0200

    Implemented SRP

--- Tap2020Demo/Tap2020Demo/Program.cs
+++ Tap2020Demo/Tap2020Demo/Program.cs
@@ -6,14 +6,15 @@ namespace Tap2020Demo
     {
         static void Main(string[] args)
         {
+            var calculator = new WithdrawalFeeCalculator();
             Account debitAccount = new DebitAccount();
             debitAccount.Deposit(100);
-            debitAccount.Withdraw(50);
+            debitAccount.Withdraw(calculator.CalculateAmountToWithdraw(debitAccount, 50));
             Console.WriteLine("Debit account: {0}.", debitAccount.Amount);
 
             Account creditAccount = new CreditAccount();
             creditAccount.Deposit(100);
-            creditAccount.Withdraw(50);
+            creditAccount.Withdraw(calculator.CalculateAmountToWithdraw(creditAccount, 50));
             Console.WriteLine("Credit account: {0}.", creditAccount.Amount);
         }
     }
--- Tap2020Demo/Tap2020Demo/Savings.cs
+++ Tap2020Demo/Tap2020Demo/Savings.cs
@@ -2,9 +2,5 @@
 {
     class Savings : Account
     {
-        protected override decimal CalculateWithdrawalFee(decimal amount)
-        {
-            return amount * .12m;
-        }
     }
 }
