3f41850556c1b766fd30a818f6b32036457664cf
RePierre <petru.rebeja@gmail.com>
2020-04-02 20:04:07 +0300

    Implemented generic repository

---
+++ Tap2020Demo/Tap2020Demo.DataAccess.SqlServer/Repositories/DataRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Uaic.Tap2020Demo.Core.Accounts;
+using Uaic.Tap2020Demo.Core.Accounts.Base;
+using Uaic.Tap2020Demo.DataAccess.Repositories;
+
+namespace Uaic.Tap2020Demo.DataAccess.SqlServer.Repositories
+{
+    public class DataRepository : IDataRepository
+    {
+        private readonly Tap2020DemoContext dataContext;
+
+        public DataRepository(Tap2020DemoContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public IQueryable<TEntity> Query<TEntity>() where TEntity : class, IEntityBase
+        {
+            return dataContext.Set<TEntity>();
+        }
+
+        public void SaveChanges()
+        {
+            dataContext.SaveChanges();
+        }
+
+        void IDataRepository.Delete<TEntity>(TEntity item)
+        {
+            var entity = dataContext.Set<TEntity>()
+                .SingleOrDefault(e => e.Id == item.Id);
+            if (entity != null)
+            {
+                dataContext.Remove(entity);
+            }
+        }
+
+        void IDataRepository.Insert<TEntity>(TEntity debitAccount)
+        {
+            dataContext.Add(debitAccount);
+        }
+
+        void IDataRepository.Update<TEntity>(TEntity debitAccount)
+        {
+            dataContext.Update(debitAccount);
+        }
+    }
+}
---
+++ Tap2020Demo/Tap2020Demo.DataAccess/Repositories/IDataRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Uaic.Tap2020Demo.Core.Accounts;
+using Uaic.Tap2020Demo.Core.Accounts.Base;
+
+namespace Uaic.Tap2020Demo.DataAccess.Repositories
+{
+    public interface IDataRepository
+    {
+        IQueryable<TEntity> Query<TEntity>() where TEntity : class, IEntityBase;
+
+        void Insert<TEntity>(TEntity debitAccount) where TEntity : class, IEntityBase;
+
+        void Update<TEntity>(TEntity debitAccount) where TEntity : class, IEntityBase;
+
+        void Delete<TEntity>(TEntity entityId) where TEntity : class, IEntityBase;
+
+        void SaveChanges();
+    }
+}
