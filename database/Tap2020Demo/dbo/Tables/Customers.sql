7bca337a08fb5dfeb26314867a56dd0990a43faa
RePierre <petru.rebeja@gmail.com>
2020-04-30 15:07:43 +0300

    Added Customers table

---
+++ database/Tap2020Demo/dbo/Tables/Customers.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Customers]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL  DEFAULT newid(), 
+    [IdNo] VARCHAR(50) NOT NULL, 
+    [FirstName] NVARCHAR(150) NOT NULL, 
+    [LastName] NVARCHAR(150) NOT NULL, 
+    [FullName] AS FirstName + ' ' + LastName, 
+    CONSTRAINT [PK_Customers] PRIMARY KEY ([Id])
+)
+
+GO
+
+CREATE UNIQUE INDEX [IX_Customers_IdNo] ON [dbo].[Customers] ([IdNo])
+
+GO
+
+CREATE INDEX [IX_Customers_FullName] ON [dbo].[Customers] ([FullName])
