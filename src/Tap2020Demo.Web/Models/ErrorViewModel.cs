9277cf8405b29b39711d67afa775aea7e1321b29
RePierre <petru.rebeja@gmail.com>
2020-05-03 18:44:28 +0300

    Added Web project

---
+++ src/Tap2020Demo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tap2020Demo.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
