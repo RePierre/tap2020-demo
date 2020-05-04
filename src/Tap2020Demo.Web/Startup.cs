d5979e5f4f53ffa1580b85bc104ae73b3b41ec38
RePierre <petru.rebeja@gmail.com>
2020-05-04 17:11:04 +0300

    Localized view

--- src/Tap2020Demo.Web/Startup.cs
+++ src/Tap2020Demo.Web/Startup.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +30,7 @@ namespace Tap2020Demo.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLocalization(/*options => options.ResourcesPath = "Resources"*/);
             services.AddControllersWithViews();
             services.AddTransient<Tap2020DemoContext>(_ =>
             {
@@ -36,6 +40,9 @@ namespace Tap2020Demo.Web
             });
             services.AddTransient<IDataRepository, DataRepository>();
             services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+            services.AddMvc()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +58,20 @@ namespace Tap2020Demo.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            var supportedCultures = new[]
+            {
+                new CultureInfo("ro-RO")
+            };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("ro-RO"),
+                // Formatting numbers, dates, etc.
+                SupportedCultures = supportedCultures,
+                // UI strings that we have localized.
+                SupportedUICultures = supportedCultures
+            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
