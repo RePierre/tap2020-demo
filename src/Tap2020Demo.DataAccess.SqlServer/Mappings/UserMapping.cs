5020c2e4fbafaafc755a7585ac0e9bdb3925b0ae
RePierre <petru.rebeja@gmail.com>
2020-05-13 14:49:58 +0300

    Added mappings for User & Role

---
+++ src/Tap2020Demo.DataAccess.SqlServer/Mappings/UserMapping.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Uaic.Tap2020Demo.Core.Identity;
+
+namespace Uaic.Tap2020Demo.DataAccess.SqlServer.Mappings
+{
+    class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users")
+                .HasKey(u => u.Id);
+        }
+    }
+}
