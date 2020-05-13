84162131f57ff065591d4877b1b283b249ba22df
RePierre <petru.rebeja@gmail.com>
2020-05-13 14:48:05 +0300

    Added ICustomerService

---
+++ src/Tap2020Demo.Core.Services/ICustomersService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uaic.Tap2020Demo.DataAccess.Repositories;
+
+namespace Uaic.Tap2020Demo.Core.Services
+{
+    public interface ICustomersService
+    {
+        Task<IEnumerable<Customer>> SearchCustomersAsync(string searchTerm);
+    }
+
+    public class CustomersService : ICustomersService
+    {
+        private readonly IDataRepository _dataRepository;
+
+        public CustomersService(IDataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+
+        public  Task<IEnumerable<Customer>> SearchCustomersAsync(string searchTerm)
+        {
+            if (String.IsNullOrEmpty(searchTerm))
+            {
+                return Task.FromResult(Enumerable.Empty<Customer>());
+            }
+
+            var results = _dataRepository.Query<Customer>()
+                .Where(c => c.FullName.Contains(searchTerm))
+                .ToArray() as IEnumerable<Customer>;
+
+            return Task.FromResult(results);
+        }
+    }
+}
