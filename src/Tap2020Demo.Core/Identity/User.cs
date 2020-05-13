4a0891998a1ea2d464ae1f992bf6e77a8fcfc922
RePierre <petru.rebeja@gmail.com>
2020-05-13 14:49:44 +0300

    Added User & Role entities

---
+++ src/Tap2020Demo.Core/Identity/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uaic.Tap2020Demo.Core.Identity
+{
+
+    public class User : IEntityBase
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string ConcurrencyStamp { get; set; }
+    }
+}
